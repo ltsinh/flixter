58492972682cfa78b29420f771b1a1f2ead94557
Ltsinh <linhtsinh@gmail.com>
2018-01-13 23:50:47 +0000

    flixter

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
-config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIAJYTZKTSDDG3BP7LA"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+  config.storage    = :aws
+  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_acl    = "public-read"
+
+  config.aws_credentials = {
+      access_key_id:     ENV["AWS_ACCESS_KEY"],
+      secret_access_key: ENV["AWS_SECRET_KEY"],
+      region:            ENV["AWS_REGION"]
   }
-  config.fog_directory  = ENV["my-bucket-name-2"]              # required
 end
