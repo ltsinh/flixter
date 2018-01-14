a00923361546a1bb0a9043698c681c2ff703b8cd
Ltsinh <linhtsinh@gmail.com>
2018-01-14 19:15:51 +0000

    flixter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113191528) do
+ActiveRecord::Schema.define(version: 20180114190252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180113191528) do
     t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video"
   end
 
   create_table "sections", force: :cascade do |t|
